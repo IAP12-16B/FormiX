8478a05920428bc9397774ecdab2bb37c8b89294
Kim Jeker <info@kije.ch>
2014-03-19 22:36:54 +0100

    Added DB helper class and globals.inc.php

---
+++ kije/Formulargenerator/inc/DB.php
@@ -0,0 +1,32 @@
+<?php
+namespace kije\Formulargenerator\inc;
+
+class DB {
+    const MYSQL_DATE = 'Y-m-d';
+    const MYSQL_DATETIME = 'Y-m-d H:i:s';
+
+    protected static $_dbh;
+
+    public static function dbh() {
+        if (!self::$_dbh) {
+            // connect to database
+            try {
+                self::$_dbh = new \PDO(
+                    'mysql:host='.DB_HOST.';dbname='.DB_DATABASE,
+                    DB_USER,
+                    DB_PASSWORD,
+                    array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+                );
+                self::$_dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            } catch (\PDOException $e) {
+                return null;
+            }
+        }
+        return self::$_dbh;
+    }
+
+    public static function unix2datetime($timestamp) {
+        return date(self::MYSQL_DATETIME, $timestamp);
+    }
+
+}
---
+++ kije/Formulargenerator/inc/globals.inc.php
@@ -0,0 +1,10 @@
+<?php
+namespace kije\Formulargenerator\inc;
+
+require_once 'db.config.php';
+require_once 'DB.php';
+
+define('FORMGEN_INC_ROOT', __DIR__);
+define('FORMGEN_ROOT', realpath(FORMGEN_INC_ROOT.'/..'));
+
+
