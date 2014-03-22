ba80ee492e67aab5961011f4311cb8ddb45d9b84
Kim Jeker <info@kije.ch>
2014-03-22 10:22:15 +0100

    output form

--- kije/Formgenerator/Tests/index.php
+++ kije/Formgenerator/Tests/index.php
@@ -1,9 +1,10 @@
-<pre><code>
-		<?php
+<pre><code><?php
+
 		require_once '../inc/globals.inc.php';
 		require_once FORMGEN_ROOT . '/Formgenerator.php';
 
-		\kije\Formgenerator\Formgenerator::init('test_form');
 
-		?>
-	</code></pre>
+		\kije\Formgenerator\Formgenerator::init('test_form');
+		echo \kije\Formgenerator\Formgenerator::getForm()->toHTML();
+
+		?></code></pre>
