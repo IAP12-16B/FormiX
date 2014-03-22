41fdea5392d5b8d4e59f82045792baa3a097998a
Kim Jeker <info@kije.ch>
2014-03-22 10:17:41 +0100

    Added Button

---
+++ kije/Formgenerator/Tags/Button.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kije
+ * Date: 3/22/14
+ * Time: 9:11 AM
+ */
+
+namespace kije\Formgenerator\Tags;
+
+require_once 'HTMLTag.php';
+
+class Button extends HTMLTag
+{
+
+	protected $_tagname = 'button';
+	protected $_selfclosing = false;
+	protected $_required_attributes = array();
+
+
+	public function __construct() {
+
+	}
+
+	public function getAllowedAttributes() {
+		return array_unique(
+			array_merge(
+				parent::getAllowedAttributes(),
+				array(
+					'autofocus',
+					'disabled',
+					'form',
+					'formaction',
+					'formenctype',
+					'formmethod',
+					'formnovalidate',
+					'formtarget',
+					'name',
+					'type',
+					'value'
+				)
+			)
+		);
+	}
+
+	/**
+	 * @param $text
+	 */
+	public function setText($text) {
+		$this->setInnerHTML($text);
+	}
+
+} 
